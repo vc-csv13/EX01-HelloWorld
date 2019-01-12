d405e1ef0eab911225a0f7b205690d3643c3ef6e
profknight <ahknight@pipeline.sbcc.edu>
2019-01-11 22:52:04 -0800

    Added main.cc

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+/**
+ * CS 140 Object-Oriented Programming Using C++
+ * CRN: [CHANGE THIS INFORMATION]
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [CHANGE THIS INFORMATION]
+ */
+
+#include <iostream>
+#include <csv13>
+
+int main(UNUSED(int argc), UNUSED(char *argv[])) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
